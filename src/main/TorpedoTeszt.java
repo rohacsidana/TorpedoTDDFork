267ab55f92baecbc5df71258580c5b9d0fff88f7
CsoltiP <csolti@szamalk-szalezi.hu>
2022-05-23 14:49:03 +0200

    TorpedoTeszt class

---
+++ src/main/TorpedoTeszt.java
@@ -0,0 +1,11 @@
+package main;
+
+public class TorpedoTeszt {
+    public static void main(String[] args) {
+        System.out.println("Teszt");
+    }
+    
+    public String tesztLoves(int poz){
+        return "";
+    }
+}
